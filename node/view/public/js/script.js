4cb3082b80aef7ef404b8e0735b2815fd10d6b1d
Saycoron <anventas4ml@gmail.com>
2018-08-13 20:05:23 +0200

    Se crean checkBox que muestran y ocultan la configuracion de registro y de mail que es completamente opcional

--- node/view/public/js/script.js
+++ node/view/public/js/script.js
@@ -12,37 +12,62 @@ const display12 = document.getElementById('senso5');
 const display13 = document.getElementById('senso6');
 
 socket.on('selec0', function (data) {
-  valor0.innerHTML = data;
+	valor0.innerHTML = data;
 });
 
 socket.on('selec1', function (data) {
-  valor1.innerHTML = data;
+	valor1.innerHTML = data;
 });
 
 socket.on('selec2', function (data) {
-  senso0.innerHTML = data;
+	senso0.innerHTML = data;
 });
 
 socket.on('selec3', function (data) {
-  senso1.innerHTML = data;
+	senso1.innerHTML = data;
 });
 
 socket.on('selec4', function (data) {
-  senso2.innerHTML = data;
+	senso2.innerHTML = data;
 });
 
 socket.on('selec5', function (data) {
-  senso3.innerHTML = data;
+	senso3.innerHTML = data;
 });
 
 socket.on('selec6', function (data) {
-  senso4.innerHTML = data;
+	senso4.innerHTML = data;
 });
 
 socket.on('selec7', function (data) {
-  senso5.innerHTML = data;
+	senso5.innerHTML = data;
 });
 
 socket.on('selec8', function (data) {
-  senso6.innerHTML = data;
+	senso6.innerHTML = data;
 });
+
+function showContent(sTipo) {
+
+	if (sTipo == "Mail") {
+		element = document.getElementById("contentMail");
+	  check = document.getElementById("checkMail");
+	  if (check.checked) {
+	    element.style.display='block';
+	  }
+	  else {
+	    element.style.display='none';
+	  }
+	}
+	
+	if (sTipo == "Registro") {
+		element = document.getElementById("contentReg");
+	  check = document.getElementById("checkReg");
+	  if (check.checked) {
+	    element.style.display='block';
+	  }
+	  else {
+	    element.style.display='none';
+	  }
+	}
+}
